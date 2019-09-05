cd1c3cec823133f1cdf5d81625cdce70ccdef50e
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-09-05 15:12:02 +0200

    Display specific infos forms.

--- lib/ekzer_interface_web/controllers/exercice_controller.ex
+++ lib/ekzer_interface_web/controllers/exercice_controller.ex
@@ -23,10 +23,13 @@ defmodule EkzerInterfaceWeb.ExerciceController do
   def specific_infos(conn, %{"level" => level, "progression" => progression, "field" => field, "consigne" => consigne} = params) do
     exercise_pid = get_session(conn, :current_exercise)
     {:ok, type} = EkzerAdd.get_exercise_value(exercise_pid, :type)
-    {:ok, :success} =EkzerAdd.add_common_infos(exercise_pid, params)
-    EkzerAdd.add_common_infos(exercise_pid, params)
+    {:ok, specific_fields} = EkzerAdd.get_exercise_value(exercise_pid, :specific_fields)
+    specific_fields_keys = Map.keys(specific_fields)
+    IO.inspect specific_fields_keys
     cond do 
-      correct_basic_infos?(level, progression, field) -> render(conn, "specific_infos.html", type: type)
+      correct_basic_infos?(level, progression, field) ->
+        {:ok, :success} = EkzerAdd.add_common_infos(exercise_pid, params)
+        display_specific_infos(conn, type)
       true -> conn |> put_flash(:error, "Certaines informations sont erronées ou manquantes.") |> redirect(to: "/add/new_exercice/basic_infos")
     end
   end
@@ -36,6 +39,23 @@ defmodule EkzerInterfaceWeb.ExerciceController do
   end
 
   #PRIVATE
+
+  defp display_specific_infos(conn, :classer) do
+    render(conn, "classer_infos.html")
+  end
+
+  defp display_specific_infos(conn, :quizz) do
+    render(conn, "quizz_infos.html")
+  end
+  defp display_specific_infos(conn, :associer) do
+    render(conn, "associer_infos.html")
+  end
+  defp display_specific_infos(conn, :prelever) do
+    render(conn, "prelever_infos.html")
+  end
+  defp display_specific_infos(conn, :completer) do
+    render(conn, "completer_infos.html")
+  end
 
   defp correct_basic_infos?(level, progression, field) do
     is_number?(level) and is_number?(progression) and field_in_list(field)
