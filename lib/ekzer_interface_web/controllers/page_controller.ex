ca3fb0cd9210355d0eeff112e14f2fd5cf1cd376
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-09-25 18:27:05 +0200

    Fix problem with renewing adder.

--- lib/ekzer_interface_web/controllers/page_controller.ex
+++ lib/ekzer_interface_web/controllers/page_controller.ex
@@ -2,17 +2,27 @@ defmodule EkzerInterfaceWeb.PageController do
   use EkzerInterfaceWeb, :controller
 
   def index(conn, _params) do
-    case get_session(conn, :adder_pid) do
-      nil -> 
+    case check_adder_alive(conn) do
+      false -> 
         {:ok, adder_pid} = EkzerAdd.add()
         conn = put_session(conn, :adder_pid, adder_pid)
+        IO.puts "adder"
+        IO.inspect get_session(conn, :adder_pid)
         render(conn, "index.html")
-        _ -> 
+      true -> 
         render(conn, "index.html")
     end
   end
 
   def new(conn, _params) do
     render(conn, "exercise_type.html")
+  end
+
+  # PRIVATE
+
+  defp check_adder_alive(conn) do
+    conn
+    |> get_session(:adder_pid)
+    |> Process.alive?
   end
 end
